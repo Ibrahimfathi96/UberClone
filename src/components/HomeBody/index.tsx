f98668d7ecd4827845a808be97845fc3651a1186
Ibrahim Fathi <himafathy796@gmail.com>
2024-06-29 07:11:17 +0300

    Refactor HomeBody component and add new views
    - Refactored HomeBody component to extract upper view into a separate component
    - Added new views for Where To and Address sections
    - Updated styles for new views and added responsive design

--- src/components/HomeBody/index.tsx
+++ src/components/HomeBody/index.tsx
@@ -6,29 +6,115 @@ import AppButton from '../AppButton';
 import styles from './styles';
 import {Images} from '../../utils/images';
 import HomeServicesFlatlist from '../HomeServicesFlatlist';
+import {Icon} from '@rneui/themed';
+import {AppColors} from '../../utils/colors';
+import {SFZ} from '../../utils/responsive';
 
 const HomeBody = () => {
   const {t} = useTranslation();
   return (
     <ScrollView bounces={false}>
-      <View style={styles.container}>
-        <Text style={styles.text1}>{t('Destress your commute')}</Text>
-        <View style={styles.view1}>
-          <View style={styles.view2}>
-            <Text style={styles.text2}>
-              {t('Read a book, Take a nap, Stare out the window')}
-            </Text>
-            <AppButton onPress={() => {}} btnText={t('Ride with Uber')} />
-          </View>
-          <View>
-            <FastImage style={styles.carUberImg} source={Images.uberCar} />
-          </View>
-        </View>
-      </View>
+      <HomeUpperView />
 
+      {/**TODO: FIX THE IMAGE INSIDE THE CARD */}
       <HomeServicesFlatlist />
+
+      <WhereToView />
+
+      <AddressView
+        addressTitle="10th Ibrahim El-attar Street"
+        addressDesc="Zagazig-Banayous"
+      />
+
+      <AddressView
+        addressTitle="El Bosta Street"
+        addressDesc="Zagazig-Train Station"
+        noBorderWidth
+      />
+
+      <Text style={styles.aroundYou}>{t('Around you')}</Text>
     </ScrollView>
   );
 };
 
 export default HomeBody;
+
+const HomeUpperView = () => {
+  const {t} = useTranslation();
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text1}>{t('Destress your commute')}</Text>
+      <View style={styles.view1}>
+        <View style={styles.view2}>
+          <Text style={styles.text2}>
+            {t('Read a book, Take a nap, Stare out the window')}
+          </Text>
+
+          <AppButton onPress={() => {}} btnText={t('Ride with Uber')} />
+        </View>
+
+        <View>
+          <FastImage style={styles.carUberImg} source={Images.uberCar} />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const WhereToView = () => {
+  const {t} = useTranslation();
+
+  return (
+    <View style={styles.view3}>
+      <Text style={styles.text3}>{t('Where to ?')}</Text>
+      <View style={styles.view4}>
+        <Icon
+          type="material-community"
+          name="clock-time-four"
+          color={AppColors.grey100}
+          size={SFZ(26)}
+        />
+
+        <Text style={styles.now}>{t('Now')}</Text>
+
+        <Icon
+          type="material-community"
+          name="chevron-down"
+          color={AppColors.grey100}
+          size={SFZ(26)}
+        />
+      </View>
+    </View>
+  );
+};
+
+const AddressView = (props: any) => {
+  const {addressTitle, addressDesc, noBorderWidth} = props;
+  return (
+    <View style={[styles.view5, noBorderWidth && styles.noBorder]}>
+      <View style={styles.view6}>
+        <View style={styles.view7}>
+          <Icon
+            type="material-community"
+            name="map-marker"
+            color={AppColors.black}
+            size={SFZ(22)}
+          />
+        </View>
+        <View>
+          <Text style={styles.addressTitle}>{addressTitle}</Text>
+          <Text style={styles.addressDesc}>{addressDesc}</Text>
+        </View>
+      </View>
+      <View>
+        <Icon
+          type="material-community"
+          name="chevron-right"
+          color={AppColors.grey50}
+          size={SFZ(26)}
+        />
+      </View>
+    </View>
+  );
+};
--- src/components/HomeBody/styles.ts
+++ src/components/HomeBody/styles.ts
@@ -1,6 +1,6 @@
 import {StyleSheet} from 'react-native';
 import {AppColors} from '../../utils/colors';
-import {SFZ, SH, SW} from '../../utils/responsive';
+import {SBR, SFZ, SH, SW} from '../../utils/responsive';
 
 const styles = StyleSheet.create({
   container: {
@@ -29,6 +29,75 @@ const styles = StyleSheet.create({
   carUberImg: {
     height: SW(100),
     width: SH(100),
+  },
+  view3: {
+    flexDirection: 'row',
+    marginTop: SH(5),
+    height: SH(50),
+    backgroundColor: AppColors.grey600,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginHorizontal: SW(15),
+    borderRadius: SBR(10),
+  },
+  text3: {
+    marginLeft: SW(15),
+    fontSize: SFZ(20),
+    color: AppColors.black,
+  },
+
+  view4: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginRight: SW(15),
+    backgroundColor: 'white',
+    paddingHorizontal: SW(10),
+    paddingVertical: SH(2),
+    borderRadius: SBR(10),
+  },
+  now: {
+    marginLeft: SW(5),
+  },
+  view5: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    paddingVertical: SH(25),
+    justifyContent: 'space-between',
+    marginHorizontal: SW(15),
+    borderBottomColor: AppColors.grey400,
+    borderBottomWidth: 1,
+  },
+  noBorder: {
+    borderBottomWidth: 0,
+  },
+  view6: {
+    flex: 5,
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  view7: {
+    backgroundColor: AppColors.grey600,
+    height: SH(40),
+    width: SW(40),
+    borderRadius: SBR(20),
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginRight: SW(20),
+  },
+  addressTitle: {
+    fontSize: SFZ(18),
+    color: AppColors.black,
+  },
+  addressDesc: {
+    color: AppColors.grey300,
+  },
+  aroundYou: {
+    fontSize: SFZ(20),
+    color: AppColors.black,
+    marginLeft: SW(20),
+    marginBottom: SH(20),
   },
 });
 
